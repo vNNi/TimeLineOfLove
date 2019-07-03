d48bf5b8a905477deb1104f94651ca670e21b84b
Vinicius <Vinicius_k2013@hotmail.com>
2019-07-02 23:33:51 -0300

    feat: add new texts

--- src/Resources/content.js
+++ src/Resources/content.js
@@ -38,8 +38,15 @@ const S2 = 'Infelizmente não temos fotos do dia do nosso primeiro beijo, mas me
 const S3 = 'Nós dois adquirimos uma intimidade muito maior nesse dia, pois eu estava começando a entender quem era você de verdade e você quem era o Vinícius, lembro que te dediquei uma música que faz sentido até hoje. Ficamos horas e horas só conversando e eu nem via o tempo passar, creio que ali eu ja estava completamente apaixonado.'
 const S3AndHalf = 'Todo dia desciamos a tuiti depois da escola, juntinhos, eu contava os minutos para a aula acabar pra eu poder passar mais uma tarde com você, e nesse dia, estavamos na frente do metrô, onde passou um rapaz vendendo rosas, eu te perguntei se você queria, tu respondeu que não e não gostava muito de flores, acha aquilo brega, porém eu vi como você continuava olhando, com uma carinha de quem queria mt ganhar, então comprei e te dei. Assim eu comecei a entender mais um tiquinho de você, que dificilmente irá falar o que realmente quer ou não, você gosta que façam, e eu adoro ver seu rostinho quando te ofereço algo que quer muito e está com vergonha de pedir!!!';
 const S4 = 'O tempo foi passando e entramos de férias, mas acredita que nunca passou nem um minuto na minha cabeça que as férias poderiam estragar o que a gente tinha? Acho que lá dentro eu ja sabia que era algo muito valioso e nada ia afastar a gente. Aqui nessa foto, foi no meu aniverśario, o primeiro que passamos juntos, você me disse que queria ir em lugar diferente, e deu a idéia de irmos na Paulista, tu sempre disse que adorava lá, e começou aqui uma coisa muito bonita que eu tenho por ti, eu quero te ajudar a conhecer e viver experiências novas, até o final da minha vida!';
-const S5 = 'In August 2010, Todd Howard revealed Bethesda were currently working on a game that had been in development since the release of Oblivion, and that progress was very far along. While the game was conceptualized after Oblivion\'s release, main development was restricted until after Fallout 3 was released. In November, a journalist from Eurogamer Denmark reported overhearing a developer on a plane talking about the project; a new The Elder Scrolls game, although Bethesda did not comment on the report. At the Spike Video Game Awards in December, Todd Howard appeared on stage to unveil a teaser trailer and announce the title of the game. The Elder Scrolls V: Skyrim was released on November 11, 2011 to widespread critical acclaim. It was awarded \'Game of the Year\' by IGN, Spike and others. The game is set after the events of Oblivion, when the great dragon Alduin the World Eater returns to Skyrim; a beast whose existence threatens all life in Tamriel. Three pieces of DLC were released on PC and Xbox 360 in 2012- Dawnguard, Hearthfire and Dragonborn, with a PlayStation 3 release in February 2013. Dawnguard added two joinable factions and an associated questline revolving around Vampires, while Hearthfire added more home customisation options. Dragonborn added the island of Solstheim to the northeast.';
-
+const S5 = 'Esse cantinho aqui é muito especial pra mim, pois me lembra o começo das nossas principais e mais profundas conversas, você fazia eu me sentir alguém que eu sempre quis ser, tu me trouxe para um mundo onde eu pensei que nunca chegaria, aquele que escreve o nome de casal nos lugares, que desce a rua de mãos dadas, que da beijo de bom dia... Tu me deu algo que ninguém nunca vai poder dar, uma sensação única';
+const S6 = 'Esse dia foi muito especial, acho que ai nós dois começavamos a sair como uma dupla, só eu e você, se encontrando para beijar e beijar, sem conseguir parar, com uma paixão que até hoje prevalece muito grande. E essa foto representa a forma que nós dois nos olhamos, eu sou completamente apaixonado nesse seu rostinho, me sinto a pessoa mais importante do mundo inteiro, tendo de cuidar de você, te dar o mundo inteirinho!';
+const S7 = 'Em janeiro, eu precisava fazer a missão mais gostosa da minha vida. Você não podia sair de casa, pois seu bilhete parava em janeiro e tu não tinha como vir até mim, ou me encontrar em outros lugares, acho que foi o mês que eu mais andei de ônibus e consequentemente, aprendi a ir pra sua casa tranquilamente. Aqui eu ainda não trabalhava, mas recebia dinheiro da minha avó, que usava tudo pra te mimar, lembro que iamos até as praças pertos da sua casa, pois não podia ficar sozinho com você na sua casa, até porque nem conhecia o PC ainda, quando chegavamos na praça, eu sempre tentava levar doce pra ti, lembro que tu adorava quando eu levava fini! e ficávamos lá, um bom tempo sentados, conversando e se beijando, até ficar tarde e eu precisar voltar.'
+const S8 = 'Essa foto representa muito pra mim, foi uma das decisões mais concisas da minha vida. Desde a última foto passaram-se 2 meses, que cá entre nós, ambos sabiam que o pedido de namoro ia rolar, acho que só estava precisando de dinheiro pra aliança. Foi ai que eu comecei a trabalhar em um buffet infantil, só pra conseguir o dinheiro pra comprar, pois até então, pra mim, era algo que não válida de nada, mesmo! Mas você mudou totalmente a minha cabeça. Uma semana antes desse dia, fui no shopping com o Yuri, vi a aliança mais bonita e menos chamativa que tinha na loja (e que coubesse no bolso hihih) e comprei, a partir dali, estava muito ansioso, pois não sabia como fazer algo que te surpreendesse... Eu sei que não consegui, pois você tinha uma certa noção e as coisas não saíram como eu imaginava, mas confesso que nesse dia, eu nunca me senti tão nervoso para você chegar em casa. Desde então, no meu sentimento, nada mudava, mas eu sabia que estava entrando em algo que para ambos era muito valioso, até porque nossas conversas sempre giraram em torno do \"Se for para namorar, eu vou casa.\" e eu sei que vou.';
+const S9 = 'Aqui nós já eramos oficialmente namorados, mas eu nunca tinha ido na sua casa de verdade, pra conversar com sua mãe, conhecer sua irmã, então lembro que você me chamou para ir comer batata frita e frangoooooo, para eu começar a me enturmar com sua família e etc. Vou te confessar que eu sentia muito medo e vergonha, não sabia como me comportar em situações assim e nunca fui em nenhum desses convites de outras pessoas no passado e, eu ja te falei algumas vezes, que eu sempre começava a furar todos os convites para sair até que as pessoas desistiam de mim, muito disso se deve a vergonha e a não me sentir a vontade com a pessoa. Mas com você foi muito, muito diferente, mesmo com medo e com vergonha, eu fui. Lembro que sai de casa umas 17h e fiquei na fila do 4001 até 19h e pouco, isso ja seria o suficiente para eu desistir e entrar naquele ciclo que comentei, porém eu sabia que você ficaria muito triste, e eu me importava e importo, muito com você e principalmente com nossa relação. Então esse dia foi muito importante para mim, pois entendi o quanto você começou a mudar minha forma de viver, inconscientemente.';
+const S10 = 'Esse dia, Lais do céu, tu me surpreendeu muito!!!!! Primeiro pela surpresa, que eu não fazia a menooooor ideia; Segundo que eu nunca imaginaria que você ia planejar algo com a minha mãe, a gente estava no começo do namoro, vocês duas tinham se falado pouquíssimas vezes!! Você enfrentou a vergonha inicial e enquanto estava na academia, covnersou com meu pai, minha mãe e fez uma supresa que eu fiquei sem acreditar... Outra coisa, eu nunca imaginei que um dia alguém fosse capaz de fazer essas coisinhas bestas por mim, nunca mesmo!';
+const S11 = 'Como eu ja tinha dito em outra parte da nossa história, você mudou a minha forma de pensar e, claro, não podia faltar essa foto! toda vez que eu penso nesse dia, eu me pergunto, qual a chance de algum dia eu ir na parada gay sem ter conhecido você, eu não teria ido, o tanto de coisas novas que a gente ja fez juntos, me encanta, e a parada foi uma delas, foi muito, muito legal e eu lembro que eu fazia mais por ti, estava indo por você, mas depois de chegar lá, eu estava por querer estar e foi muito legal!';
+const S12 = 'Aaaah, esse dia eu estava encantado! Eu conheci uma menina totalmente diferente pela qual eu tinha visto a primeira vez e mesmo assim, eu não conseguia tirar o olho dela! Você ficou muito feliz em mudar o cabelo e eu lembro até hoje o quanto se sentia KKKKKKKKKKKK. Você tinha conseguido ficar mais linda do que o normal, simplesmente por ter mudado. E essa foto é importante, para te lembrar, que independente de roupa, tênis, cabelo, olho, nariz, qualquer coisa física, eu vou estar sempre com você e te achando a mulher mais maravilhosa do mundo todo ! Pois a beleza que tu tem, é de outro mundo';
+const S13 = 'Essa aqui foi uma das melhores épocas da minha vida, eu conseguia te ver quase que todo dia da semana, ficavamos deitadinhos de tarde, dando muitos beijinhos, sem preocupações nenhuma sobre a vida, nosso amor era o que eu mais vivia. Já tinhamos uma grande intimidade, lembro de pegar no seu pé para você estudar, eu te ajudando com física, tu indo lá e tirando 10 !!!!! Nós dois, sempre davamos um jeitinho de comemorar os dias 17, ainda damos, mas antes era tudo sem nenhum dinheiro, podia ser uma tarde no ceret, ou uma tarde no sofá de casa, vendo filme!';
 
 const data = [
   {
@@ -90,56 +97,56 @@ const data = [
     photo: img6,
     title: 'The Elder Scrolls: 5',
     subtitle: 'Skyrim',
-    content: S5
+    content: S6
   },
   {
     date: '01/11/2017',
     photo: img7,
     title: 'The Elder Scrolls: 5',
     subtitle: 'Skyrim',
-    content: S5
+    content: S7
   },
   {
     date: '03/17/2017',
     photo: img8,
     title: 'The Elder Scrolls: 5',
     subtitle: 'Skyrim',
-    content: S5
+    content: S8
   },
   {
     date: '04/17/2016',
     photo: img9,
     title: 'The Elder Scrolls: 5',
     subtitle: 'Skyrim',
-    content: S5
+    content: S9
   },
   {
     date: '06/10/2017',
     photo: img10,
     title: 'The Elder Scrolls: 5',
     subtitle: 'Skyrim',
-    content: S5
+    content: S10
   },
   {
     date: '06/18/2017',
     photo: img11,
     title: 'The Elder Scrolls: 5',
     subtitle: 'Skyrim',
-    content: S5
+    content: S11
   },
   {
     date: '07/31/2017',
     photo: img12,
     title: 'The Elder Scrolls: 5',
     subtitle: 'Skyrim',
-    content: S5
+    content: S12
   },
   {
     date: '10/17/2017',
     photo: img13,
     title: 'The Elder Scrolls: 5',
     subtitle: 'Skyrim',
-    content: S5
+    content: S13
   },
   {
     date: '11/14/2017',
