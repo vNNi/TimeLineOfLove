97de774b2da2d99772ffea31e7dfea0d677be32a
Vinicius <Vinicius_k2013@hotmail.com>
2019-07-05 18:46:57 -0300

    feat: adjust in service worker

---
+++ src/sw.js
@@ -0,0 +1,32 @@
+var appFiles = [
+    './index.html',
+    '/song/*',
+    './404.html',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-256x256.png',
+    '/icons/icon-512x512.png',
+    '/icons/touch-icon-iphone.png'
+]
+appFiles.concat(self.__precacheManifest)
+
+self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open('TLLOve').then(function(cache){
+            return cache.addAll(appFiles)
+        }).catch(function(e){
+            console.log('error in install app', e);
+        })
+    )
+});
+self.addEventListener('fetch', function(event) {
+
+    event.respondWith(
+    caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+
+});
