4b2a199a7d9a1dc0db57e0a1adb98ad1df177b7b
Vinicius <Vinicius_k2013@hotmail.com>
2019-07-05 16:50:44 -0300

    fix: remove effet hook and add autplay html5

--- src/Components/Sound/index.js
+++ src/Components/Sound/index.js
@@ -3,19 +3,10 @@ import React, {useEffect} from 'react';
 export default function index(props) {
 
     const audio = React.createRef();
-
-    useEffect(()=>{
-       let playPromise =  audio.current.play();
-       if(playPromise != undefined){
-           playPromise.then((_)=>{
-           }).catch((e)=>{
-                console.log('music error', e);
-           })
-       }
-    },[])
+    
     return (
         <>
-            <audio ref={audio} controls loop style={{display:'none'}}>
+            <audio ref={audio} preload="none" autoPlay controls loop style={{display:'none'}}>
                 <source src={props.video} type="audio/mpeg"></source>
             </audio>
         </>
