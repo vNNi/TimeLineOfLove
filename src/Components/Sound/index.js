95c344e0a098206afa07b001b2018b2238b3783d
Vinicius <Vinicius_k2013@hotmail.com>
2019-07-05 17:19:29 -0300

    fix: back with autoplay audio approach

--- src/Components/Sound/index.js
+++ src/Components/Sound/index.js
@@ -3,10 +3,19 @@ import React, {useEffect} from 'react';
 export default function index(props) {
 
     const audio = React.createRef();
-    
+
+    useEffect(()=>{
+       let playPromise =  audio.current.play();
+       if(playPromise != undefined){
+           playPromise.then((_)=>{
+           }).catch((e)=>{
+                console.log('music error', e)
+           })
+       }
+    },[])
     return (
         <>
-            <audio ref={audio} preload="none" autoPlay controls loop style={{display:'none'}}>
+            <audio ref={audio} controls loop autoPlay style={{display: 'none'}}>
                 <source src={props.video} type="audio/mpeg"></source>
             </audio>
         </>
