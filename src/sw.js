efa54978d14cef1d969df8b23bc456d98bb9d79e
Vinicius <Vinicius_k2013@hotmail.com>
2019-08-31 11:51:05 -0300

    chore: up all to git

--- src/sw.js
+++ src/sw.js
@@ -22,7 +22,6 @@ self.addEventListener('install', function(e){
     )
 });
 self.addEventListener('fetch', function(event) {
-
     event.respondWith(
     caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
